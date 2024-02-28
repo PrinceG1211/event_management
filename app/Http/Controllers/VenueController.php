ad780af5f940391ccc833fc829605690f3babb43
Prince <princegohel915@gmail.com>
2024-02-28 13:18:24 +0530

    VenueController.php

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller as Controller;
 use App\Models\Venue;
+use App\Models\image;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Validator;
@@ -13,10 +14,10 @@ class VenueController extends Controller
 
     public function index(): JsonResponse
     {
-        $Venue = Venue::where('isActive', 1)->with('PackageDetail')->get()->each(function ($Venue) {
-           
-          });
-        $Venue = Venue::where('isActive', 1)->get();
+        $Venue = Venue::where('isActive', 1)->with('PackageDetail',)->get()->each(function ($Venue) {
+            $Venue->packageName = $Venue->PackageDetail->packageName;  
+            $Venue->setHidden(['PackageDetail']); 
+         });
         if ($Venue != null) {
             return $this->sendResponse('success', $Venue, 'Venue Found.');
         } else {
@@ -30,7 +31,7 @@ class VenueController extends Controller
         $input = $request->all();
 
         $validator = Validator::make($input, [
-            
+      
             'venueName' => 'required',
             'capacity' => 'required',
             'contactPerson' => 'required',
@@ -42,7 +43,6 @@ class VenueController extends Controller
             'city' => 'required',
             'area' => 'required',
             'packageID' => 'required',
-            
         ]);
 
         if ($validator->fails()) {
@@ -50,26 +50,30 @@ class VenueController extends Controller
         }
 
         $Venue = new Venue();
-        $Venue->venueID = $request->post('venueID');
-        $Venue->venueName = $request->post('bname');
-        $Venue->capacity = $request->post('vendorName');
+        
+        
+        $Venue->venueName = $request->post('venueName');
+        $Venue->capacity = $request->post('capacity');
         $Venue->contactPerson = $request->post('contactPerson');
         $Venue->email = $request->post('email');
         $Venue->mobileNo = $request->post('mobileNo');
         $Venue->address = $request->post('address');
-        $Venue->image = $request->post('image');
+        $image= $request->file('image');
+        $Venue->image= $this->uploadimage($image, "Venue");
         $Venue->price = $request->post('price');
-        $Venue->city = $request->post('category');
-        $Venue->area = $request->post('category');
+        $Venue->city = $request->post('city');
+        $Venue->area = $request->post('area');
         $Venue->packageID = $request->post('packageID');
+        
         $Venue->save();
 
+        
         return $this->sendResponse('success', $Venue->venueID, 'Venue Added successfully.');
     }
 
     public function show($id): JsonResponse
     {
-        $Venue = Venue::where('isActive', 1)->where('venueID', $id)->first();
+        $Venue = Venue::where('isActive', 1)->where('VenueID', $id)->first();
 
         if (is_null($Venue)) {
             return $this->sendResponse('failure', $Venue, 'No Venue Found.');
@@ -82,6 +86,8 @@ class VenueController extends Controller
         $input = $request->all();
 
         $validator = Validator::make($input, [
+
+            'venueID' => 'required',
             'venueName' => 'required',
             'capacity' => 'required',
             'contactPerson' => 'required',
@@ -93,27 +99,30 @@ class VenueController extends Controller
             'city' => 'required',
             'area' => 'required',
             'packageID' => 'required',
+           
         ]);
 
         if ($validator->fails()) {
             return $this->sendResponse('failure', 'Validation Error.', $validator->errors());
         }
 
-        $id = $request->post('venueID');
+        $id = $request->post('VenueID');
         $Venue = Venue::find($id);
         if ($Venue != null) {
-            $Venue->venueID = $request->post('venueID');
-            $Venue->venueName = $request->post('bname');
-            $Venue->capacity = $request->post('vendorName');
+            $Venue->VenueID = $request->post('VenueID');
+            $Venue->venueName = $request->post('venueName');
+            $Venue->capacity = $request->post('capacity');
             $Venue->contactPerson = $request->post('contactPerson');
             $Venue->email = $request->post('email');
             $Venue->mobileNo = $request->post('mobileNo');
             $Venue->address = $request->post('address');
-            $Venue->image = $request->post('image');
+            $image= $request->file('image');
+            $Venue->image= $this->uploadimage($image, "Venue");
             $Venue->price = $request->post('price');
-            $Venue->city = $request->post('category');
-            $Venue->area = $request->post('category');
+            $Venue->city = $request->post('city');
+            $Venue->area = $request->post('area');
             $Venue->packageID = $request->post('packageID');
+            $updated = $Venue->save();
             if ($updated == 1) {
                 return $this->sendResponse('success', $updated, 'Venue updated successfully.');
             } else {
@@ -128,7 +137,7 @@ class VenueController extends Controller
 
     public function delete(Request $request): JsonResponse
     {
-        $id = $request->post('venueID');
+        $id = $request->post('VenueID');
         $Venue = Venue::find($id);
         if ($Venue != null) {
             $Venue->isActive = 0;
@@ -140,6 +149,23 @@ class VenueController extends Controller
             }
         } else {
             return $this->sendResponse('failure', $Venue, 'Venue Not Found.');
+        }
+    }
+
+    public function deleteimages(Request $request): JsonResponse
+    {
+        $id = $request->post('imageID');
+        $image= Venueimage::find($id);
+        if ($image!= null) {
+            $deleted = $this->deleteimage($image->image);
+            if ($deleted) {
+                $image->delete();
+                return $this->sendResponse('success', $updated, 'imageDeleted successfully.');
+            } else {
+                return $this->sendResponse('failure', $updated, ' imageNot delete.');
+            }
+        } else {
+            return $this->sendResponse('failure', $image, ' imageNot Found.');
         }
     }
 }
